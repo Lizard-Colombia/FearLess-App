cc02702483431db4e7db82b359f0c0aedffe439a
Lizard-Colombia <71155299+Lizard-Colombia@users.noreply.github.com>
2020-09-29 18:55:36 -0500

    Update app.js
    Added in home-page logic

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import WelcomeMessage from "./welcome-message";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const onUserNameChange = (event) => {
+    console.log(event.target.value);
+    setUserName(event.target.value);
+  };
   return (
     <main>
       <h1>FearLess</h1>
       <WelcomeMessage greeting="Welcome" name="Sam I Am"></WelcomeMessage>
-      {/* <WelcomeMessage greeting="Good Morning" name="Class"></WelcomeMessage> */}
       <h3>
         Let's begin by creating a list of fears that you would like to work on
         together.
       </h3>
+      <form>
+        <label>
+          Username:{" "}
+          <input type="text" value={userName} onChange={onUserNameChange} />
+        </label>
+        <input type="submit" />
+      </form>
     </main>
   );
 }
