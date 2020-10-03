7f9aa0ec429d7506bd28b22400ecdc6c161ec78a
Lizard-Colombia <71155299+Lizard-Colombia@users.noreply.github.com>
2020-10-03 08:29:50 -0500

    10.2.2020-lateNight
    ToDo Form List etc... to be changed to Fear List

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import WelcomeMessage from "./welcome-message";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import Todo from "./Todo";
 
 function App() {
   const [userName, setUserName] = useState("");
-  // const [fearName, setFearName] = fearName("");
   const onUserNameChange = (event) => {
     setUserName(event.target.value);
-    // const onFearName = (event) => {
-    //   setFearName(event.target.value);
-    // };
   };
+
+  const [todos, setTodos] = useState([]);
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+
   return (
     <main>
       <header>
@@ -48,37 +53,38 @@ function App() {
         </label>
         <input type="submit" />
       </form>
-
       {/* have the form now Welcome the user - by name -  */}
-
       <h2>Hi, {userName}! It is nice to meet you.</h2>
       <h3>
         We are now going to build a list of fears that we can work on together.
       </h3>
+      {/* React To Do List from Video */}
+      <p> React ToDo List</p>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} />
       {/* form for entering in fears */}
-      <h2>
-        <b>Instructions: </b> List a word or brief description of your fear in
-        the large field and then list a number between 1-10 in the small field
-        to show the power of this fear.{" "}
-        <em>Example: 1 is a mild fear and 10 is totally over-powering.</em>
-        <form>
-          <ul>
-            <li>
-              <label>
-                Fear: {""}
-                <input type="text" />
-              </label>
-            </li>
-            <li>
-              <label>
-                Rank {""}
-                <input type="number" />
-              </label>
-            </li>
-          </ul>
-        </form>
-        <h2>Text input fields</h2>
-      </h2>
+      <p></p>
+      <b>Instructions: </b> List a word or brief description of your fear in the
+      large field and then list a number between 1-10 in the small field to show
+      the power of this fear.{" "}
+      <em>Example: 1 is a mild fear and 10 is totally over-powering.</em>
+      <form>
+        <ul>
+          <li>
+            <label>
+              Fear: {""}
+              <input type="text" />
+            </label>
+          </li>
+          <li>
+            <label>
+              Rank {""}
+              <input type="number" />
+            </label>
+          </li>
+        </ul>
+      </form>
+      <h2>Text input fields</h2>
       <footer>
         <div>
           <button>Progress</button>
--- src/components/edit-item.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// Edit item should get the list item the user has selected and allow them to edit the description box and rank
-function editItem() {
-  return <editItem></editItem>;
-}
-
-export default editItem;
