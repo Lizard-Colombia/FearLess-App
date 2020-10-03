76e4e4dc4e97dc3b769c6bbe0ccbe64ec77d05f7
Lizard-Colombia <71155299+Lizard-Colombia@users.noreply.github.com>
2020-10-02 22:10:47 -0500

    10.2 - 10 pm
    Began adding a "ToDo"List that I will alter to become the "FearLess" list.

---
+++ src/components/TodoForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import uuid from "uuid";
+
+function TodoForm({ addTodo }) {
+  const [todo, setTodo] = useState({
+    id: "",
+    task: "",
+    completed: false,
+  });
+
+  function handleTaskInputChange(e) {
+    // e.target.value contains new input from onChange
+    // event for input elements
+    setTodo({ ...todo, task: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault(); // prevents browser refresh
+    // trim() gets rid of string whitespace
+    if (todo.task.trim()) {
+      addTodo({ ...todo, id: uuid.v4() });
+      setTodo({ ...todo, task: "" });
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="task"
+        type="text"
+        value={todo.task}
+        onChange={handleTaskInputChange}
+      />
+      <button type="submit" />
+    </form>
+  );
+}
+
+export default TodoForm;
